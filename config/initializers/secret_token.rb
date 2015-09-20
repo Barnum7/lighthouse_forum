e8687c64cfa5f28d4a98124ef96a60e9cf428c24
Brian Groat <Barnum7@users.noreply.github.com>
2015-09-20 12:01:52 -0400

    First commit. Created new forum directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '1de9767edcaaa93f8c25303cd21fdafcb1e16fc3fc4d9d240fb34599a14b8f27f9c21d50609e4908b9b69d5ea7835cbc82993d7857094575225d51c24f69963d'
