e66282b54fb7ae77f348b3c1258c83d3bb2d21f5
Brian Groat <Barnum7@users.noreply.github.com>
2015-09-21 14:31:07 -0400

    Built functionality to display posts, post temlate, and seed posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,35 @@
 class PostsController < ApplicationController
 
 	def index
-
+		@posts = [
+			{
+				title:"Superhumanly attractive",
+				author: "Beautiful women around the world",
+				text: <<-eos.gsub(/\s+/, " ").strip
+				Brian Groat is possible the single most beautiful
+				human being on the planet. My God. I should just blind
+				myself now so that I don't have to see anything that
+				isn't him.
+				eos
+				},
+			{
+				title: "Unparalleled Brilliance",
+				author: "Leonardo da Vinci",
+				text: <<-eos.gsub(/\s+/, " ").strip
+				Boy did I feel dull sitting next to this guy at
+				school. He's just one of those guys who gets it you
+				know? Art, music, sciences. The dude can just do it all.
+				I wish I were more like him and not such a worthless piece
+				of garbage.	
+				eos
+				},
+			{
+				title: "Pretty Average Guy",
+				author: "Somebody ugly and stupid",
+				text: <<-eos.gsub(/\s+/, " ").strip
+				Pretty ordinary.. He's alright I guess
+				eos
+				}]
 	end
 	
 end
